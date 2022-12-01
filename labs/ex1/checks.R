d990fa387c3c6ee478c12df879bdc109d349a155
Pontus Höjer <pontushojer@gmail.com>
2022-12-01 14:56:11 +0100

    Update checks.R

--- labs/ex1/checks.R
+++ labs/ex1/checks.R
@@ -1,27 +1,56 @@
-check_exercise_2 <- function(sequence, answer) {
-  
-  key <- cb(sequence)
-  names(answer) <- toupper(answer)
-  names(key) <- toupper(key)
-  
-  in_names <- as.vector(sapply(names(answer),
-                        function(x){
-                          x %in% names(key)
-                        }))
-  if (all(in_names)) {
-    ordr_ans <- sort(names(answer))
-    ordr_key <- sort(names(key))
-  
-    status <- all(answer[ordr_ans] == key[ordr_key])
-    
-    if (status) {
-      print("Congrats you did it!")
-    } else {
-      print("These are not the correct numbers...")
+
+catch_error <- function(expr) {
+  # Catch errors thrown by check and output message to student to try again.
+  tryCatch(
+    expr,
+    error = function(e) {
+      warning("Your solution raises the following error:\n")
+      print(e$message)
+      warning("Try again!")
     }
-  } else {
-    print("seems like you did not format you answer correctly!")
+  )
+}
+
+check_exercise_1 <- function(vec) {
+  check <- function(vec) {
+    if (round(vec %*% c(1337,69,42,666,80085)) == 151677) {
+      print("Great job! The answer is correct.")
+    } else {
+      warning("Your answer is sadly not correct, try again.")
+    }
   }
+  
+  catch_error(check(vec))
+}
+
+
+check_exercise_2 <- function(sequence, answer) {
+  check <- function(sequence, answer) {
+    key <- cb(sequence)
+    names(answer) <- toupper(answer)
+    names(key) <- toupper(key)
+
+    in_names <- as.vector(sapply(names(answer),
+                          function(x){
+                            x %in% names(key)
+                          }))
+    if (all(in_names)) {
+      ordr_ans <- sort(names(answer))
+      ordr_key <- sort(names(key))
+
+      status <- all(answer[ordr_ans] == key[ordr_key])
+
+      if (status) {
+        print("Congrats you did it!")
+      } else {
+        warning("These are not the correct numbers...")
+      }
+    } else {
+      warning("seems like you did not format you answer correctly!")
+    }
+  }
+
+  catch_error(check(sequence, answer))
 }
 
 cb <- function(sequence) {
@@ -33,50 +62,53 @@ cb <- function(sequence) {
 }
 
 check_exercise_3 <- function(ans){
-  a1 <- decrypt(362026828)
-  a2 <- decrypt(746595)
-  a3 <- c("hqvj333334ba-45",
-          "hqvj333334bab6a",
-          "hqvj3333355a586",
-          "hqvj333334baaa9",
-          "huffc33463")
-  
-  a3 <- sapply(a3,
-               d_caesar)
-  
-  a3 <- as.vector(toupper(a3))
-  
-  key <- list(a1 = a1, a2 = a2, a3 = a3)
-  score <- c()
-  
-  format_error <- FALSE
-  if (!(all(sapply(names(ans),
-                   function(x){ x %in% names(key)})))) {
-    format_error <- TRUE
-  }
-  
-  if (length(ans[["a3"]]) != length(key[["a3"]])) {
-    format_error <- TRUE 
-  } else if (!(all(sapply(names(ans[["a3"]]),
-                          function(x){ x %in% names(key[["a3"]])})))) {
-    format_error <- TRUE
-  }
-  
-  if (format_error) {
-    print("Format error, try again!")
-  } else {
-  
-    for (k in c("a1","a2","a3")) {
-      score <- c(score,all(ans[[k]] == key[[k]]))
+  check <- function(ans) {
+    a1 <- decrypt(362026828)
+    a2 <- decrypt(746595)
+    a3 <- c("hqvj333334ba-45",
+            "hqvj333334bab6a",
+            "hqvj3333355a586",
+            "hqvj333334baaa9",
+            "huffc33463")
+    
+    a3 <- sapply(a3,
+                 d_caesar)
+    
+    a3 <- as.vector(toupper(a3))
+    
+    key <- list(a1 = a1, a2 = a2, a3 = a3)
+    score <- c()
+    
+    format_error <- FALSE
+    if (!(all(sapply(names(ans),
+                     function(x){ x %in% names(key)})))) {
+      format_error <- TRUE
     }
-    if (all(score)) {
-      print("well done, all answers are correct!")
+    
+    if (length(ans[["a3"]]) != length(key[["a3"]])) {
+      format_error <- TRUE 
+    } else if (!(all(sapply(names(ans[["a3"]]),
+                            function(x){ x %in% names(key[["a3"]])})))) {
+      format_error <- TRUE
+    }
+    
+    if (format_error) {
+      print("Format error, try again!")
     } else {
-      for (f in which(score == F)) {
-        print(sprintf("Question %d is incorrect, try again!",f))
+    
+      for (k in c("a1","a2","a3")) {
+        score <- c(score,all(ans[[k]] == key[[k]]))
+      }
+      if (all(score)) {
+        print("well done, all answers are correct!")
+      } else {
+        for (f in which(score == F)) {
+          print(sprintf("Question %d is incorrect, try again!",f))
+        }
       }
     }
   }
+  catch_error(check(ans))
 }
 
 encrypt <- function(x) {
